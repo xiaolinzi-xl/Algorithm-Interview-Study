daebc8ad74aaee0308f0eabd57af722c4fc207a0
xiaolinzi-xl <daoshangxl@163.com>
2018-10-22 16:25:35 +0800

    优化leetcode_283算法

--- 03-array/leetcode_283.py
+++ 03-array/leetcode_283.py
@@ -12,6 +12,18 @@ class Solution:
         for i in range(nonzero_idx, len(nums)):
             nums[i] = 0
 
+    def moveZeroes_2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k = 0  # [0,k) 全是非0元素
+        # [k,i） 全为0
+        for i in range(0, len(nums)):
+            if nums[i] != 0:
+                nums[k], nums[i] = nums[i], nums[k]
+                k += 1
+
 
 '''
 时间复杂度：O(n)
@@ -22,8 +34,11 @@ class Solution:
 def main():
     test = Solution()
     arr = [0, 1, 0, 3, 12]
+    arr2 = arr.copy()
     test.moveZeroes(arr)
+    test.moveZeroes_2(arr2)
     print(arr)
+    print(arr2)
 
 
 if __name__ == '__main__':
